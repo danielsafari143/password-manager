eacd9cb014aeb6c723ac622e4e74e529897ca790
unknown <danielsafari143@gmail.com>
2024-06-07 14:10:02 +0200

    ADD CHANGE

---
+++ app/_layout.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native-web"
+
+export default Layout = () => {
+    return (
+        <View>
+            <Text>Welcom to the app </Text>
+        </View>
+    )
+}
